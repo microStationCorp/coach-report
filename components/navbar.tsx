64b59cceda05aecc99f53591417256508eca8509
sujan mondal <sujanmondal916@gmail.com>
2023-04-24 18:07:45 +0530

    responsive nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,29 +1,92 @@
+import Head from "next/head";
 import Link from "next/link";
+import { useState } from "react";
 
 export const Navbar = () => {
+  const [navbar, setNavbar] = useState(false);
+
   return (
-    <div
-      className={
-        "p-4 bg-gradient-to-l from-slate-400 to-slate-200 flex justify-between items-center"
-      }
-    >
-      <div className="text-2xl">
-        <Link href={"/"} className="hover:font-semibold">
-          Coach-report
-        </Link>
-      </div>
-      <div className="text-slate-100 flex space-x-2">
-        <div className="capitalize">
-          <Link href="/add_coach" passHref className="hover:font-semibold">
-            add coach
-          </Link>
+    <>
+      <div
+        className={"bg-gradient-to-l from-slate-400 to-slate-200 w-full shadow"}
+      >
+        <div className="md:flex justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:px-8">
+          <div>
+            <div className="flex items-center justify-between py-3 md:py-5 md:block">
+              <div className="text-2xl font-bold">
+                <Link href={"/"} className="hover:font-semibold">
+                  Coach-report
+                </Link>
+              </div>
+              <div className="md:hidden">
+                <button
+                  className="p-2 rounded-md outline-none focus:border-gray-400 focus:border"
+                  onClick={() => setNavbar(!navbar)}
+                >
+                  {navbar ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 text-white"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 text-white"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+          {/* menu */}
+          <div>
+            <div
+              className={`flex-1 justify-self-center pb-3 mt-2 md:block md:pb-0 md:mt-0 ${
+                navbar ? "block" : "hidden"
+              }`}
+            >
+              <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                <div className="capitalize font-semibold">
+                  <Link
+                    href="/add_coach"
+                    passHref
+                    className="hover:font-semibold"
+                  >
+                    add coach
+                  </Link>
+                </div>
+                <div className="capitalize font-semibold">
+                  <Link
+                    href="/coach_list"
+                    passHref
+                    className="hover:font-semibold"
+                  >
+                    coach list
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="capitalize">
-          <Link href="/coach_list" passHref className="hover:font-semibold">
-            coach list
-          </Link>
-        </div>
       </div>
-    </div>
+    </>
   );
 };
