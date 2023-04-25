35a32da875beb10b1c108dfe5be405802b52be6a
sujan mondal <sujanmondal916@gmail.com>
2023-04-26 00:36:01 +0530

    coach page progressing

--- pages/coach/[id]/index.tsx
+++ pages/coach/[id]/index.tsx
@@ -1,12 +1,31 @@
+import { supabase } from "@/utils/supabaseClient";
+import { CoachDataI } from "@/utils/types";
 import Head from "next/head";
 
-export default function CoachPage({ id }: { id: string }) {
+export default function CoachPage({ data }: { data: CoachDataI }) {
   return (
     <>
       <Head>
-        <title>Cach</title>
+        <title>
+          Coach - {data.coach_number} ({data.coach_type})
+        </title>
       </Head>
-      <div>coach page {id}</div>
+      <div className="bg-slate-300 rounded-md">
+        <div className="p-4">
+          <div>
+            <CustomItem label="base" data={data.base} />
+          </div>
+          <div>
+            <CustomItem label="rake type" data={data.rake_type} />
+          </div>
+          <div>
+            <CustomItem label="coach number" data={data.coach_number} />
+          </div>
+          <div>
+            <CustomItem label="coach type" data={data.coach_type} />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
@@ -16,7 +35,37 @@ export async function getServerSideProps({
 }: {
   params: { id: string };
 }) {
-  return {
-    props: { id: params.id },
-  };
+  const { data, error } = await supabase
+    .from("coach_list")
+    .select("*")
+    .eq("id", params.id);
+
+  if (data) {
+    return {
+      props: { data: data[0] },
+    };
+  } else {
+    return {
+      data: {},
+    };
+  }
 }
+
+const CustomItem = ({
+  label,
+  data,
+}: {
+  label: string;
+  data: string | null;
+}) => {
+  return (
+    <>
+      <span>{label}</span>:{" "}
+      {data ? (
+        <span>{data}</span>
+      ) : (
+        <span className="text-red-500 font-semibold">Not provided</span>
+      )}
+    </>
+  );
+};
