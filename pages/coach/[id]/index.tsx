f043d40a89b72ca252753a9a7f655c7bad8d0be2
sujan mondal <sujanmondal916@gmail.com>
2023-05-04 21:39:30 +0530

    single coach page modified

--- pages/coach/[id]/index.tsx
+++ pages/coach/[id]/index.tsx
@@ -1,14 +1,16 @@
 import { supabase } from "@/utils/supabaseClient";
-import { CoachDataI, LHBTable, SGTable } from "@/utils/types";
+import { CoachDataI, DamageCoachI, LHBTable, SGTable } from "@/utils/types";
 import Head from "next/head";
 import { CustomItem } from "@/components/customItem";
 
 export default function CoachPage({
   data,
   equipment_table,
+  damage_list,
 }: {
   data: CoachDataI;
   equipment_table: SGTable | LHBTable;
+  damage_list: DamageCoachI[];
 }) {
   return (
     <>
@@ -49,6 +51,39 @@ export default function CoachPage({
               ))}
           </div>
         </div>
+        <div className="sm:pl-4 pb-4">
+          <div className="text-2xl underline">Damages :</div>
+          <div className=" text-cyan-800">Total no. of damages : {damage_list.length}</div>
+          {damage_list.map((dl) => (
+            <div key={dl.id}>
+              <div className="p-2">
+                <div>
+                  <span className="capitalize font-semibold">date :</span>
+                  <span>
+                    {new Date(dl.created_at!).toLocaleString("en-GB")}
+                  </span>
+                </div>
+                <div>
+                  <span className="capitalize font-semibold">rake:</span>
+                  <span>{dl.rake}</span>
+                </div>
+                <div>
+                  <span className="capitalize font-semibold">
+                    damage type :
+                  </span>
+                  <span className="capitalize text-slate-500 font-semibold">
+                    {dl.damage_type}
+                  </span>
+                </div>
+                <div>
+                  <span className="capitalize font-semibold">description:</span>
+                  <span className="capitalize">{dl.description}</span>
+                </div>
+              </div>
+              <hr />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
@@ -65,13 +100,19 @@ export async function getServerSideProps({
     .eq("id", params.id);
 
   if (data) {
+    const { data: damage_list, error } = await supabase
+      .from("damage_data")
+      .select("id , created_at, rake , description, damage_type")
+      .eq("coach", params.id)
+      .order("created_at", { ascending: false });
+
     if (data[0].rake_type?.toUpperCase() === "SG") {
       const { data: eq_table, error } = await supabase
         .from("sg_coach_table")
         .select("*")
         .eq("coach", params.id);
       return {
-        props: { data: data[0], equipment_table: eq_table![0] },
+        props: { data: data[0], equipment_table: eq_table![0], damage_list },
       };
     } else {
       const { data: eq_table, error } = await supabase
@@ -79,7 +120,7 @@ export async function getServerSideProps({
         .select("*")
         .eq("coach", params.id);
       return {
-        props: { data: data[0], equipment_table: eq_table![0] },
+        props: { data: data[0], equipment_table: eq_table![0], damage_list },
       };
     }
   } else {
